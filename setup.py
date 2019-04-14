a41512f46026dfb8789411a83e460a1ba37890d7
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-04-14 14:53:41 -0700

    build script for PiPy

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gcGroupbyExtension-gcalmettes",
+    version="0.0.1",
+    author="Guillaume Calmettes",
+    author_email="gcalmettes@g.ucla.edu",
+    description="Allows to construct a pipeline of functions to be applied independently on the groups of a groupby object.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gcalmettes/pandas-groupby-apply-chaining-extension",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
